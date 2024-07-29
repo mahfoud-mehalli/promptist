19700ec58675fb439aa9a93eae623b1cc0a6b730
mahfoud-mehalli <mahfoudmehalli12345@gmail.com>
2024-07-29 19:06:14 +0100

    Project done ☑️

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -10,7 +10,7 @@ const PromptCardList = ({ data, handleTagClick }) => {
       {data.map((post) => (
         <PromptCard 
           key={postMessage._id}
-          post={post }
+          post={post}
           handleTagClick={handleTagClick}
         />
       ))}
@@ -21,11 +21,12 @@ const PromptCardList = ({ data, handleTagClick }) => {
 
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState('')
   const [posts, setPosts] = useState([])
 
-  const handleSearchChange = (e) => {
-  }
+  // Search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -35,6 +36,36 @@ const Feed = () => {
     }
     fetchPosts();
   },[])
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
   
   return (
     <section className="feed">
@@ -48,10 +79,15 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PromptCardList 
-        data={posts}
-        handleTagClick={() => {}}
-      />
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
   )
 }
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -17,45 +17,58 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     navigator.clipboard.writeText(post.prompt)
     setTimeout(() => setCopied(''), 3000 )
   }
+
+  const handleProfileClick = () => {
+    console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
   
   return (
-    <div className="prompt_card">
-      <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-          <Image 
+    <div className='prompt_card'>
+      <div className='flex justify-between items-start gap-5'>
+        <div
+          className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+          onClick={handleProfileClick}
+        >
+          <Image
             src={post.creator.image}
-            alt={post.creator.username}
+            alt='user_image'
             width={40}
             height={40}
-            className="rounded-full object-contain"
+            className='rounded-full object-contain'
           />
-          <div className="flex flex-col">
-            <h3 className="font-satoshi font-semibold text-gray-900">
+
+          <div className='flex flex-col'>
+            <h3 className='font-satoshi font-semibold text-gray-900'>
               {post.creator.username}
             </h3>
-            <p className="font-inter text-sm text-gray-500">
+            <p className='font-inter text-sm text-gray-500'>
               {post.creator.email}
             </p>
           </div>
         </div>
-        <div className="copy_btn" onClick={handleCopy}>
-          <Image 
-            src={copied === post.prompt
-              ? '/assets/icons/tick.svg'
-              : '/assets/icons/copy.svg'
+
+        <div className='copy_btn' onClick={handleCopy}>
+          <Image
+            src={
+              copied === post.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
             }
-            alt="Copy"
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
             width={12}
             height={12}
           />
         </div>
       </div>
-      <p className="my-4 font-satoshi text-sm text-gray-700">
-        {post.prompt}
-      </p>
-      <p 
-        className="font-inter text-sm blue_gradient cursor-pointer"
-        onClick={() => {handleTagClick && handleTagClick(post.tag)}}
+
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+      <p
+        className='font-inter text-sm blue_gradient cursor-pointer'
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
         #{post.tag}
       </p>
@@ -76,9 +89,8 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           </p>
         </div>
       )}
-      
     </div>
-  )
+  );
 }
 
 export default PromptCard
